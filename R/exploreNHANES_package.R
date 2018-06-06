242370f3256e37d62a306f8a50ec163e5dc74241
Jankowiak Justyna <justyna.jankowiak@grupawp.pl>
2018-06-06 13:25:54 +0200

    adding package description in help

---
+++ R/exploreNHANES_package.R
@@ -0,0 +1,18 @@
+#' @title Explore NHANES data
+#'
+#' @description
+#' \pkg{exploreNHANES} is the \R package created to explore a subset of NHANES
+#' publication, drawing attention to the prevalence of exercise and associated
+#' health effects across different demographics. The functions provided with
+#' a package allow to explore and visualize data.
+#'
+#' @name exploreNHANES-package
+#' @rdname exploreNHANES-package
+#' @aliases exploreNHANES
+#' @docType package
+#' @author Justyna Jankowiak
+#'
+#' @references
+#' \emph{\pkg{exploreNHANES} Package homepage}, \url{https://github.com/jjankowiak/exploreNHANES}
+#'
+"_PACKAGE"
